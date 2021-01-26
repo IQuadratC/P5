3640ae0f01692d28506ce242a91bcabfe3372cba
Stroby241 <maarten.behn@gmail.com>
2021-01-26 12:17:10 +0100

    Handy App

---
+++ Moblie/Mobile test/Assets/Utility/Variables/BoolVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility.Variables
+{
+    [CreateAssetMenu(fileName = "BoolVariable", menuName = "Utility/Varibles/Bool")]
+    public class BoolVariable : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [NonSerialized]
+        public float Value;
+        public float InitialValue;
+        
+        public void OnBeforeSerialize() { }
+        public void OnAfterDeserialize()
+        {
+            Value = InitialValue;
+        }
+    }
+}
---
+++ Moblie/Mobile test/Assets/Utility/Variables/IntVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility.Variables
+{
+    [CreateAssetMenu(fileName = "IntVariable", menuName = "Utility/Varibles/Int")]
+    public class IntVariable : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [NonSerialized]
+        public float Value;
+        public float InitialValue;
+        
+        public void OnBeforeSerialize() { }
+        public void OnAfterDeserialize()
+        {
+            Value = InitialValue;
+        }
+    }
+}
