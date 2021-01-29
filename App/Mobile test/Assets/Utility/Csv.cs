c4a57d12ea0052fb0dd40a1dc4eda09bac8980cd
Stroby241 <maarten.behn@gmail.com>
2021-01-29 15:31:27 +0100

    Get Map of Lidar Data

---
+++ App/Mobile test/Assets/Utility/Csv.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+namespace Utility
+{
+    public static class Csv
+    {
+        public static List<List<string>> ParseCVSFile(string csvString)
+        {
+            List<List<string>> csvData = new List<List<string>>();
+            string[] lines = csvString.Split('\n');
+            for (var i = 0; i < lines.Length; i++)
+            {
+                csvData.Add(new List<string>());
+                string[] fields = lines[i].Split(',',';');
+                for (var j = 0; j < fields.Length; j++)
+                {
+                    csvData[i].Add(fields[j].Replace("\r",""));
+                }
+            }
+
+            return csvData;
+        } 
+    }
+}
