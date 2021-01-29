5097ad1376d295e1894a543eb662013085691cb4
Stroby241 <maarten.behn@gmail.com>
2021-01-29 13:06:29 +0100

    CSV Parser

---
+++ App/Mobile test/Assets/Utility/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Utility
+{
+    public static class FileHelper
+    {
+        public static string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static void Write(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+    }
+}
