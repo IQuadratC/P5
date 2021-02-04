a486ce9be0d5e31bea143451b1edce6c00d6b67a
Stroby241 <maarten.behn@gmail.com>
2021-02-04 14:11:41 +0100

    Cleaning up Code

---
+++ App/Mobile test/Assets/Scripts/Lidar/LidarPoint.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Utility;
+
+namespace Lidar
+{
+    public class LidarPoint
+    {
+        public Dictionary<int, Vector2> positions;
+        
+        public LidarPoint()
+        {
+            positions = new Dictionary<int, Vector2>();
+            distances = new List<int>[360];
+        }
+        private List<int>[] distances;
+        
+        public void LoadCSVData(string name)
+        {
+            List<List<string>> csvData = Csv.ParseCVSFile(File.ReadAllText(Application.dataPath + "\\" + name));
+            
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = new List<int>();
+            }
+            
+            foreach (var line in csvData)
+            {
+                int index = (int) float.Parse(line[0]);
+                int data = int.Parse(line[1]);
+                if (data != 0)
+                {
+                    distances[index].Add(data);
+                }
+            }
+            
+            UpdatePositions();
+        }
+        public void AddData(int[,] data)
+        {
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                distances[data[i,0]].Add(data[i,1]);
+            }
+            
+            UpdatePositions();
+        }
+
+        private void UpdatePositions()
+        {
+            for (int i = 0; i < distances.Length; i++)
+            {
+                int sum = 0;
+                foreach (var distance in distances[i])
+                {
+                    sum += distance;
+                }
+                if (sum > 0)
+                {
+                    sum /= distances[i].Count;
+                }
+                else
+                {
+                    continue;
+                }
+
+                float x = (float)(Math.Sin(i * Math.PI / 180) * sum);
+                float y = (float)(Math.Cos(i * Math.PI / 180) * sum);
+                positions[i] = new Vector2(x,y);
+            }
+        }
+    }
+}
--- App/Mobile test/Assets/Scripts/Lidar/ProcessLidarData.cs
+++ App/Mobile test/Assets/Scripts/Lidar/ProcessLidarData.cs
@@ -7,13 +7,6 @@ using Utility;
 
 namespace Lidar
 {
-    public struct LidarPoint
-    {
-        public List<int>[] distances;
-        public int[] averageDistances;
-        public Vector2[] positions;
-    }
-    
     public class ProcessLidarData : MonoBehaviour
     {
         public GameObject whitespherePreFab;
@@ -26,9 +19,12 @@ namespace Lidar
         
         private void Start()
         {
-            LidarPoint lidarPoint = LoadLidarPoint("Testdata_notMove.csv");
-            LidarPoint lidarPoint1 = LoadLidarPoint("Testdata_Move_40cm.csv");
+            LidarPoint lidarPoint = new LidarPoint();
+            lidarPoint.LoadCSVData("Testdata_notMove.csv");
             
+            LidarPoint lidarPoint1 = new LidarPoint();
+            lidarPoint1.LoadCSVData("Testdata_Move_40cm.csv");
+
             Vector2[] averagePositions = new Vector2[360];
             Vector2 offset = Vector2.zero;
             for (int i = 0; i < 360; i++)
@@ -38,12 +34,12 @@ namespace Lidar
             }
             offset /= 360;
 
-            foreach (Vector2 lidarPointPosition in lidarPoint.positions)
+            foreach (Vector2 lidarPointPosition in lidarPoint.positions.Values)
             {
                 GameObject o = Instantiate(redspherePreFab, lidarPointPosition / 100, Quaternion.identity);
                 o.transform.SetParent(redPartent.transform, true);
             }
-            foreach (Vector2 lidarPointPosition in lidarPoint1.positions)
+            foreach (Vector2 lidarPointPosition in lidarPoint1.positions.Values)
             {
                 GameObject o = Instantiate(bluespherePreFab, lidarPointPosition / 100, Quaternion.identity);
                 o.transform.SetParent(bluePartent.transform, true);
@@ -56,53 +52,6 @@ namespace Lidar
             Instantiate(whitespherePreFab, offset / 100, Quaternion.identity);
 
             bluePartent.transform.position += new Vector3(offset.x / 100, offset.y / 100, 0);
-        }
-
-        public LidarPoint LoadLidarPoint(string name)
-        {
-            List<List<string>> csvData = Csv.ParseCVSFile(File.ReadAllText(Application.dataPath + "\\" + name));
-            
-            LidarPoint lidarPoint = new LidarPoint();
-            lidarPoint.distances = new List<int>[360];
-            for (int i = 0; i < lidarPoint.distances.Length; i++)
-            {
-                lidarPoint.distances[i] = new List<int>();
-            }
-            
-            foreach (var line in csvData)
-            {
-                int index = (int) float.Parse(line[0]);
-                int data = int.Parse(line[1]);
-                if (data != 0)
-                {
-                    lidarPoint.distances[index].Add(data);
-                }
-            }
-
-            lidarPoint.averageDistances = new int[360];
-            for (int i = 0; i < lidarPoint.distances.Length; i++)
-            {
-                int sum = 0;
-                foreach (var distance in lidarPoint.distances[i])
-                {
-                    sum += distance;
-                }
-                if (sum != 0)
-                {
-                    lidarPoint.averageDistances[i] = sum / lidarPoint.distances[i].Count;
-                }
-            }
-
-            lidarPoint.positions = new Vector2[360];
-            for (int i = 0; i < lidarPoint.averageDistances.Length; i++)
-            {
-                int distance = lidarPoint.averageDistances[i];
-                float x = (float)(Math.Sin(i * Math.PI / 180) * distance);
-                float y = (float)(Math.Cos(i * Math.PI / 180) * distance);
-                lidarPoint.positions[i] = new Vector2(x,y);
-            }
-
-            return lidarPoint;
         }
     }
 }
