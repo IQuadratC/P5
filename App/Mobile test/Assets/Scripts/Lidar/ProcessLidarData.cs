e16c933b681e28d15ad1dc0f4916b9216e5a0c3b
Stroby241 <maarten.behn@gmail.com>
2021-02-04 13:12:57 +0100

    Fist try on Linear Movement

--- App/Mobile test/Assets/Scripts/Lidar/ProcessLidarData.cs
+++ App/Mobile test/Assets/Scripts/Lidar/ProcessLidarData.cs
@@ -7,7 +7,7 @@ using Utility;
 
 namespace Lidar
 {
-    struct LidarPoint
+    public struct LidarPoint
     {
         public List<int>[] distances;
         public int[] averageDistances;
@@ -16,10 +16,51 @@ namespace Lidar
     
     public class ProcessLidarData : MonoBehaviour
     {
-        public GameObject spherePreFab;
+        public GameObject whitespherePreFab;
+        public GameObject redspherePreFab;
+        public GameObject bluespherePreFab;
+        public GameObject greenspherePreFab;
+        public GameObject redPartent;
+        public GameObject bluePartent;
+        public GameObject greenPartent;
+        
         private void Start()
         {
-            List<List<string>> csvData = Csv.ParseCVSFile(File.ReadAllText(Application.dataPath + "\\lidartestdata.csv"));
+            LidarPoint lidarPoint = LoadLidarPoint("Testdata_notMove.csv");
+            LidarPoint lidarPoint1 = LoadLidarPoint("Testdata_Move_40cm.csv");
+            
+            Vector2[] averagePositions = new Vector2[360];
+            Vector2 offset = Vector2.zero;
+            for (int i = 0; i < 360; i++)
+            {
+                averagePositions[i] = lidarPoint.positions[i] - lidarPoint1.positions[i];
+                offset += averagePositions[i];
+            }
+            offset /= 360;
+
+            foreach (Vector2 lidarPointPosition in lidarPoint.positions)
+            {
+                GameObject o = Instantiate(redspherePreFab, lidarPointPosition / 100, Quaternion.identity);
+                o.transform.SetParent(redPartent.transform, true);
+            }
+            foreach (Vector2 lidarPointPosition in lidarPoint1.positions)
+            {
+                GameObject o = Instantiate(bluespherePreFab, lidarPointPosition / 100, Quaternion.identity);
+                o.transform.SetParent(bluePartent.transform, true);
+            }
+            foreach (Vector2 lidarPointPosition in averagePositions)
+            {
+                GameObject o = Instantiate(greenspherePreFab, lidarPointPosition / 100, Quaternion.identity);
+                o.transform.SetParent(greenPartent.transform, true);
+            }
+            Instantiate(whitespherePreFab, offset / 100, Quaternion.identity);
+
+            bluePartent.transform.position += new Vector3(offset.x / 100, offset.y / 100, 0);
+        }
+
+        public LidarPoint LoadLidarPoint(string name)
+        {
+            List<List<string>> csvData = Csv.ParseCVSFile(File.ReadAllText(Application.dataPath + "\\" + name));
             
             LidarPoint lidarPoint = new LidarPoint();
             lidarPoint.distances = new List<int>[360];
@@ -59,9 +100,9 @@ namespace Lidar
                 float x = (float)(Math.Sin(i * Math.PI / 180) * distance);
                 float y = (float)(Math.Cos(i * Math.PI / 180) * distance);
                 lidarPoint.positions[i] = new Vector2(x,y);
-
-                Instantiate(spherePreFab, lidarPoint.positions[i] / 100, Quaternion.identity);
             }
+
+            return lidarPoint;
         }
     }
 }
