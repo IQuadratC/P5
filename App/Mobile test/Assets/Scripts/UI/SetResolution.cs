d44290ea760eeaa660ebac8d7bbae61039e686dc
Niko4u5 <niko4u@fam-email.de>
2021-02-06 17:15:53 +0100

    implemented first layout of the app

---
+++ App/Mobile test/Assets/Scripts/UI/SetResolution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour
+{
+    public Camera renderCamera;
+    public RenderTexture texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        texture.height = renderCamera.pixelHeight;
+        texture.width = renderCamera.pixelWidth - 400;
+    }
+}
