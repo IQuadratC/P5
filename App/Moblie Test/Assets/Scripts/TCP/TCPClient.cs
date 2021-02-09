d4806452bfe9758ddcad00df8e63b26c5e0138d5
Stroby241 <maarten.behn@gmail.com>
2021-02-09 20:44:21 +0100

    New TCP Client

--- App/Moblie Test/Assets/Scripts/TCP/TCPClient.cs
+++ App/Moblie Test/Assets/Scripts/TCP/TCPClient.cs
@@ -3,16 +3,16 @@ using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 using Utility.Events;
+using Utility.Variables;
 
-namespace UI
+namespace TCP
 {
     public class TCPClient : MonoBehaviour
     {
-        public GameEvent sendEvent;
-        public string sendMessage;
+        public StringVariable sendMessage;
         
         public GameEvent reciveEvent;
-        public string reciveMessage;
+        public StringVariable reciveMessage;
         
         TcpClient client;
         byte[] bytes = new byte[4096];
@@ -25,48 +25,55 @@ namespace UI
         {
             // Connect to the remote server. The IP address and port # could be
             // picked up from a settings file.
-            client.Connect("95.89.112.92", 54000);
-
+            try
+            {
+                client.Connect("95.89.112.92", 54000);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Failed to Connect to Server!");
+                throw;
+            }
+            
             // Start reading the socket and receive any incoming messages
             client.GetStream().BeginRead(bytes,
                 0,
                 bytes.Length,
                 MessageReceived,
                 null);
-
-            Send("Hallo");
         }
 
         private void MessageReceived(IAsyncResult ar)
         {
-            if (ar.IsCompleted)
+            if (!ar.IsCompleted) return;
+            // End the stream read
+            int bytesIn = client.GetStream().EndRead(ar);
+            if (bytesIn > 0)
             {
-                // End the stream read
-                var bytesIn = client.GetStream().EndRead(ar);
-                if (bytesIn > 0)
-                {
-                    // Create a string from the received data. For this server 
-                    // our data is in the form of a simple string, but it could be
-                    // binary data or a JSON object. Payload is your choice.
-                    var tmp = new byte[bytesIn];
-                    Array.Copy(bytes, 0, tmp, 0, bytesIn);
-                    var str = Encoding.ASCII.GetString(tmp);
-                    Debug.Log(str);
-                }
-                // Clear the buffer and start listening again
-                Array.Clear(bytes, 0, bytes.Length);
-                client.GetStream().BeginRead(bytes,
-                    0,
-                    bytes.Length,
-                    MessageReceived,
-                    null);
+                // Create a string from the received data. For this server 
+                // our data is in the form of a simple string, but it could be
+                // binary data or a JSON object. Payload is your choice.
+                byte[] tmp = new byte[bytesIn];
+                Array.Copy(bytes, 0, tmp, 0, bytesIn);
+                string str = Encoding.ASCII.GetString(tmp);
+                    
+                reciveMessage.Value = str;
+                reciveEvent.Raise();
+                Debug.Log(str);
             }
+            // Clear the buffer and start listening again
+            Array.Clear(bytes, 0, bytes.Length);
+            client.GetStream().BeginRead(bytes,
+                0,
+                bytes.Length,
+                MessageReceived,
+                null);
         }
 
-        private void Send(string text)
+        public void Send()
         {
             // Encode the message and send it out to the server.
-            var msg = Encoding.ASCII.GetBytes(text);
+            byte[] msg = Encoding.ASCII.GetBytes(sendMessage.Value);
             client.GetStream().Write(msg, 0, msg.Length);
         }
     }
---
+++ App/Moblie Test/Assets/Scripts/TCP/TestButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility.Events;
+using Utility.Variables;
+
+public class TestButton : MonoBehaviour
+{
+    public InputField InputField;
+    public GameEvent Event;
+    public StringVariable text;
+    
+    public void Send()
+    {
+        text.Value = InputField.text;
+        Event.Raise();
+    }
+}
