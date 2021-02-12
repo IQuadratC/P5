19f4c0462759cb5d8a2ec58002ef2f0358c7611c
Niko4u5 <niko4u@fam-email.de>
2021-02-12 13:50:14 +0100

    removed python tcp socket and improved multi threading

--- Roboter/RasPI/recive.py
+++ Roboter/RasPI/recive.py
@@ -1,14 +1,12 @@
-import socket
 import serial
 from math import sqrt
 import threading
-import time
 
 sidewaysMultiplier = 10
-steepPerMeter = 1000
-millisecondsPerMeter = 20000
+steepPerCM = 1000
+moveSpeed = 20000  # milliseconds per step
 stepsPerDegree = 1
-rotationMillisecondPerSteep = 20
+rotationSpeed = 20  # milliseconds per step
 stopMultiThread = 0
 
 ser = serial.Serial(
@@ -31,40 +29,28 @@ def send(msg):
 
 def read(msg):
     global stopMultiThread
-    if msg == "forward":
-        move(0xffff / steepPerMeter, 0)
-    elif msg == "backward":
-        move(-0xffff / steepPerMeter, 0)
-    elif msg == "right":
-        move(0, 0xffff/(steepPerMeter*sidewaysMultiplier))
-    elif msg == "left":
-        move(0, -0xffff/(steepPerMeter*sidewaysMultiplier))
-    elif msg == "rotateLeft":
-        rotate(0xffff / stepsPerDegree)
-    elif msg == "rotateRight":
-        rotate(-0xffff / stepsPerDegree)
-    elif msg == "stop":
+    stopMultiThread = stopMultiThread + 1
+    if msg == "stop":
         send((0).to_bytes(13, "big"))
     elif msg.split(" ", 1)[0] == "move":
-        move(float(msg.split(" ", 2)[1]), float(msg.split(" ", 2)[2]))
+        move(float(msg.split(" ", 2)[1]), float(msg.split(" ", 3)[2]), float(msg.split(" ", 3)[2]))
     elif msg.split(" ", 1)[0] == "rotate":
         rotate(float(msg.split(" ", 1)[1]))
     elif msg.split(",", 1)[0] == "multi":
-        stopMultiThread = 0
         thread = threading.Thread(target=multi, args=(msg.split(",", 1)[1],))
         thread.start()
 
 
-def move(x, y): # x, y in meters
-    m1 = steepPerMeter * x
-    m2 = steepPerMeter * x
-    m3 = steepPerMeter * x
-    m4 = steepPerMeter * x
-    m1 -= y * steepPerMeter * sidewaysMultiplier
-    m2 += y * steepPerMeter * sidewaysMultiplier
-    m3 += y * steepPerMeter * sidewaysMultiplier
-    m4 -= y * steepPerMeter * sidewaysMultiplier
-    time = millisecondsPerMeter * sqrt(x*x+y*y)
+def move(x, y, speed): # x, y in meters
+    m1 = steepPerCM * x
+    m2 = steepPerCM * x
+    m3 = steepPerCM * x
+    m4 = steepPerCM * x
+    m1 -= y * steepPerCM * sidewaysMultiplier
+    m2 += y * steepPerCM * sidewaysMultiplier
+    m3 += y * steepPerCM * sidewaysMultiplier
+    m4 -= y * steepPerCM * sidewaysMultiplier
+    time = speed * sqrt(x * x + y * y)
     if m1 != 0:
         m1speed = int(time/abs(m1))
     else:
@@ -119,49 +105,18 @@ def rotate(a):  # a in degrees
         int(abs(stepsPerDegree * a)).to_bytes(2, "big", signed=False) +  # 2 Bytes Motor 2 Steps
         int(abs(stepsPerDegree * a)).to_bytes(2, "big", signed=False) +  # 2 Bytes Motor 3 Steps
         int(abs(stepsPerDegree * a)).to_bytes(2, "big", signed=False) +  # 2 Bytes Motor 4 Steps
-        int(rotationMillisecondPerSteep).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 1 Speed
-        int(rotationMillisecondPerSteep).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 2 Speed
-        int(rotationMillisecondPerSteep).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 3 Speed
-        int(rotationMillisecondPerSteep).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 4 Speed
+        int(rotationSpeed).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 1 Speed
+        int(rotationSpeed).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 2 Speed
+        int(rotationSpeed).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 3 Speed
+        int(rotationSpeed).to_bytes(1, "big", signed=False) +  # 1 Byte Motor 4 Speed
         (directions).to_bytes(1, "big", signed=False)  # 1.-4. Bit Motor 1-4 direction
     )
 
 
 def multi(msg):
+    stop = stopMultiThread
     for i in msg.split(","):
         read(i)
         ser.read(size=1)  # wait until movement is done
-        if stopMultiThread == 1:
+        if stopMultiThread != stop:
             return
-
-
-# take the server name and port name
-host = 'local host'
-port = 5000
-
-# create a socket at server side using TCP / IP protocol
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# bind the socket with server and port number
-s.bind(('', port))
-
-# allow maximum 1 connection to the socket
-s.listen(1)
-
-while True:
-    # wait till a client accept connection
-    c, addr = s.accept()
-
-    # display client address
-    print("CONNECTION FROM:", str(addr))
-
-    # receive message string from server, at a time 1024 B
-    msg = c.recv(1024)
-    while msg:
-        stopMultiThread = 1
-        read(msg.decode("utf-8"))
-        print(msg.decode("utf-8"))
-        msg = c.recv(1024)
-
-    # disconnect the server
-    c.close()
