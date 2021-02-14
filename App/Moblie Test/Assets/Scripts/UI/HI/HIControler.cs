3df00834155e4787e04f15c578a59ea311ced800
Niko4u5 <niko4u@fam-email.de>
2021-02-14 20:54:19 +0100

    added speed to HI

--- App/Moblie Test/Assets/Scripts/UI/HI/HIControler.cs
+++ App/Moblie Test/Assets/Scripts/UI/HI/HIControler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Utility.Events;
 using Utility.Variables;
 
@@ -10,7 +11,7 @@ public class HIControler : MonoBehaviour
     [SerializeField]private float maxSpeedDirection;
     [SerializeField]private float speedDirection;
     [SerializeField]private float minDirectionChange;
-    [SerializeField]private float maxSpeedRotation;
+    [SerializeField]private float speedRotation;
     [SerializeField]private float minRotationChange;
     private float2 lastDirection;
     private float lastRotation;
@@ -26,23 +27,35 @@ public class HIControler : MonoBehaviour
         float2 frameDirection = direction.Value;
         if (math.abs(frameRotation - lastRotation) > minRotationChange)
         {
-            sendString.Value = "rotate " + frameRotation;
+            sendString.Value = "rotate " + math.sign(frameRotation) * 180 + " " + (math.abs(frameRotation) * speedRotation);
             sendEvent.Raise();
             lastRotation = frameRotation;
         }
         else if (!lastRotation.Equals(0f) && frameRotation.Equals(0f))
         {
-            sendString.Value = "rotate " + frameRotation;
+            sendString.Value = "stop";
             sendEvent.Raise();
             lastRotation = frameRotation;
-        }else if (math.abs(math.length(frameDirection - lastDirection)) > minDirectionChange)
+            
+        }
+        else if (math.abs(math.length(frameDirection - lastDirection)) > minDirectionChange && !frameDirection.Equals(float2.zero))
         {
-            sendString.Value = "move " + frameDirection;
-            sendEvent.Raise();
-            lastDirection = frameDirection;
-        }else if (!lastDirection.Equals(float2.zero) && frameDirection.Equals(float2.zero))
+            if (math.length(frameDirection) < maxSpeedDirection)
+            {
+                sendString.Value = "move " + (math.normalize(frameDirection) * 1000) + " " + (math.length(frameDirection) * speedDirection);
+                sendEvent.Raise();
+                lastDirection = frameDirection;
+            }
+            else
+            {
+                sendString.Value = "move " + (math.normalize(frameDirection) * 1000) + " " + speedDirection;
+                sendEvent.Raise();
+                lastDirection = frameDirection;
+            }
+        }
+        else if (!lastDirection.Equals(float2.zero) && frameDirection.Equals(float2.zero))
         {
-            sendString.Value = "move " + frameDirection;
+            sendString.Value = "stop";
             sendEvent.Raise();
             lastDirection = frameDirection;
         }
