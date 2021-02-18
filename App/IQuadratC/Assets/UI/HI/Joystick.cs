e246ac00ba8f8bf1f25ec3a30bf4c43708b47c7d
Stroby241 <maarten.behn@gmail.com>
2021-02-18 16:22:38 +0100

    Merging

--- App/IQuadratC/Assets/UI/HI/Joystick.cs
+++ App/IQuadratC/Assets/UI/HI/Joystick.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using Utility.Events;
 using Utility.Variables;
 
 public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     
-    [SerializeField]private Transform stick;
-
-    [SerializeField]private Transform basis;
-
-    [SerializeField]private Vec2Variable direction;
+    [SerializeField] private Transform stick;
+    [SerializeField] private Transform basis;
+    [SerializeField] private Vec2Variable direction;
+    [SerializeField] private Camera cam;
     
     private bool pressed;
     
@@ -29,15 +29,23 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         if (pressed)
         {
-            Vector2 touch = Input.GetTouch(0).position;
-            touch = Camera.main.ScreenToWorldPoint(touch);
-            stick.position = ((Vector3)touch);
-            direction.Value = (Vector2)basis.position - touch;
+            float3 point;
+            if (Input.touches.Length > 0)
+            {
+                point = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+            }
+            else
+            {
+                point = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            stick.localPosition = new float3(point.x,point.y,0);
+            direction.Value = new float2(basis.position.x - point.x, basis.position.y - point.y);
         }
         else
         {
             direction.Value = float2.zero;
-            stick.position = basis.position;
+            stick.localPosition = float3.zero;
         }
     }
 }
--- App/IQuadratC/Assets/UI/HI/SliderControler.cs
+++ App/IQuadratC/Assets/UI/HI/SliderControler.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Utility.Variables;
 
-public class SliderControler : MonoBehaviour
+public class SliderControler : MonoBehaviour, IPointerUpHandler
 {
     [SerializeField]private Slider mainSlider;
     [SerializeField]private FloatVariable rotation;
-    public void Update()
-    {
-        if (Input.touchCount == 0)
-        {
-            mainSlider.value = 0;
-            rotation.Value = 0;
-        }
+    
+    public void OnPointerUp(PointerEventData eventData){
+        mainSlider.value = 0;
+        rotation.Value = 0;
     }
+    
+    
 }
