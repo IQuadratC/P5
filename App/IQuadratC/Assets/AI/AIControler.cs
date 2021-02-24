8b2bdaeb571c23105c583ffb40395b812f75256f
Niko4u5 <niko4u@fam-email.de>
2021-02-24 20:28:01 +0100

    made circle check for obsticales and send tcp message

--- App/IQuadratC/Assets/AI/AIControler.cs
+++ App/IQuadratC/Assets/AI/AIControler.cs
@@ -18,12 +18,12 @@ public class AIControler : MonoBehaviour
     [SerializeField]private Int2ListVariable goalsInput;
     [SerializeField]private Int2ListVariable obstaclesPointsInput;
     [SerializeField]private Int2ListVariable pathOutput;
-    [SerializeField]private FloatVariable radius;
     
     // don't change these after start
     [SerializeField]private int distanceToWalls; 
     [SerializeField]private float speed;
     [SerializeField]private bool useRotation;
+    [SerializeField]private bool driveCircle;
     private Dictionary<int2, int> obstacles;
     private List<int2> circle;
     
@@ -43,25 +43,6 @@ public class AIControler : MonoBehaviour
         }
     }
 
-    public void SetCircleGoals()
-    {
-        List<int2> goals = new List<int2>(); 
-        float2 pos = positionInput.Value.xy;
-        float2 goal = goalsInput.Value[0];
-        float2 vec = pos - goal;
-        float r = math.distance(goal, pos);
-        for (int i = 0; i < 8 * r; i++)
-        {
-            float2 newPos = mathAdditions.Rotate(vec, 360 * i / r);
-            if (!goals.LastOrDefault().Equals((int2)newPos))
-            {
-                goals.Add((int2)newPos);
-            }
-        }
-
-        pathOutput.Value = goals;
-    }
-    
     /**
      * Starts processing thread
      */
@@ -86,7 +67,11 @@ public class AIControler : MonoBehaviour
         UpdateObsticals();
     
         string msg;
-        if (useRotation)
+        if (driveCircle)
+        {
+            msg = Circle();
+        }
+        else if (useRotation)
         {
             msg = WithRotation();
         }
@@ -94,7 +79,11 @@ public class AIControler : MonoBehaviour
         {
             msg = NoRotation();
         }
-        
+
+        if (msg == null)
+        {
+            return;
+        }
         Threader.RunOnMainThread(ParsePath);
         void ParsePath()
         {
@@ -199,6 +188,60 @@ public class AIControler : MonoBehaviour
 
         return msg;
     }
+    
+    public String Circle()
+    {
+        float2 goal = goals[0];
+        float2 vec = pos.xy - goal;
+        float r = math.distance(goal, pos.xy);
+        for (int i = 0; i < 8 * r; i++)
+        {
+            float2 newPos = mathAdditions.Rotate(vec, 360 * i / r);
+            if (!path.LastOrDefault().Equals((int2)newPos))
+            {
+                path.Add((int2)(newPos + pos.xy));
+                
+                if (obstacles.ContainsKey((int2)(newPos + pos.xy))) {path = new List<int2>(); return null;}
+            }
+        }
+
+        pathOutput.Value = path;
+        
+        String msg = "roboter multi ";
+        float2 old = pos.xy;
+        float2 oldRotation = mathAdditions.Rotate(new float2(0,1), pos.z);
+        float2 move;
+        for (int i = 0; i < path.Count; i++)
+        {
+            if (i >= 1 && i < (path.Count - 1) &&
+                ((path[i - 1] + new int2(2, 0)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(0, 2)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(-2, 0)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(0, -2)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(-2, 2)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(-2, -2)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(2, 2)).Equals(path[i + 1]) ||
+                 (path[i - 1] + new int2(2, -2)).Equals(path[i + 1])))
+            {
+                continue;
+            }
+            move =  (path[i]) - old;
+            if (!move.Equals(float2.zero))
+            {
+                msg += "rotate," + (int)mathAdditions.Angle(oldRotation, move);
+                msg += "move," + (int)math.length(move) + ";0;" + speed + ",";
+                oldRotation = (path[i]) - old;
+            }
+            old = (path[i]);
+        }
+        move = old - (path[path.Count - 1].xy);
+        if (!move.Equals(float2.zero))
+        {
+            msg += "move," + move.x + ";" + move.y + ",";
+        }
+
+        return msg;
+    }
     private void UpdateObsticals()
     {
         obstacles = new Dictionary<int2, int>();
@@ -216,14 +259,7 @@ public class AIControler : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
-        if (obstacles != null)
-        {
-            Gizmos.color = Color.red;
-            foreach (var point in obstacles)
-            {
-                Gizmos.DrawCube(new Vector3(point.Key.x, point.Key.y), new Vector3(1,1,1));
-            }
-        }
+        
         Gizmos.color = Color.green;
         foreach (int2 point in pathOutput.Value)
         {
