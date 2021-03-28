40f4af3f0d054781c7d58b9fc1fc5e74880a3d65
nordlicht28 <yesenia@moehring-family.de>
2021-03-28 16:45:26 +0200

    made multi work

--- Simulation/Simulation/Assets/StringInterpreter.cs
+++ Simulation/Simulation/Assets/StringInterpreter.cs
@@ -6,18 +6,44 @@ using UnityEngine;
 public class  StringInterpreter : MonoBehaviour
 {
     [SerializeField]private Movment script;
-    
-    public void PassString(String input)
+    private List<String[]> Actions = new List<string[]>();
+
+    private void FixedUpdate()
     {
+        if (Actions.Count == 0)
+        {
+            return;
+        }
+        
+        if (Actions[0][0] == "move")
+        {
+            PassMove(Actions[0][1]);
+        }
+        else if (Actions[0][0] == "rotate")
+        {
+            PassRotation(Actions[0][1]);
+        }
+        
+        Actions.RemoveAt(0);
+    }
+
+    public void PassString(String input)
+    { 
+        Actions = new List<string[]>();
+        
         if (input.Split(' ') [0] == "roboter")
         {
             if (input.Split(' ')[1] == "move")
             {
-                PassMove(input.Split(' ')[2]);
+                Actions.Add(new  String[] {"move" , (input.Split(' ')[2])});
             }
             else if (input.Split(' ')[1] == "rotate")
             {
-                PassRotation(input.Split(' ')[2]);
+                Actions.Add(new  String[] {"rotate" , (input.Split(' ')[2])});
+            }
+            else if (input.Split(' ')[1] == "multi")
+            {
+                PassMulti(input.Split(' ')[2]);
             }
         }
     }
@@ -32,4 +58,30 @@ public class  StringInterpreter : MonoBehaviour
         float rot = (int.Parse(input.Split(',')[0]));
         script.Rotate(rot);
     }
+    private void PassMulti(String input)
+    {
+        input = input.Replace(',', ' ');
+        input = input.Replace(';', ',');
+        input = input.Replace(':', ';');
+        input = input.Replace('|', ':');
+
+        for (int i = 0; i < input.Split(' ').Length - 1; i += 2)
+        {
+            if (input.Split(' ')[i] == "move")
+            {
+                Actions.Add(new  String[] {"move" , (input.Split(' ')[i + 1])});
+            }
+            else if (input.Split(' ')[i] == "rotate")
+            {
+                Actions.Add(new  String[] {"rotate" , (input.Split(' ')[i + 1])});
+            }
+            else if (input.Split(' ')[i] == "multi")
+            {
+                PassMulti(input.Split(' ')[i + 1]);
+            }
+        }
+        
+        
+    }
+    
 }
