a22df81b31c2eddd57a2fa632f0ad5548ef68e4c
Stroby241 <maarten.behn@gmail.com>
2021-06-09 21:35:00 +0200

    Anhang

---
+++ Anhang/Abstranktions Sytem durch Game Events und Globale Varibalen Datein/Global Variablen/Int2ListVariable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Utility.Variables
+{
+    [CreateAssetMenu(fileName = "Int2List", menuName = "Utility/Varibles/Int2List")] 
+    public class Int2ListVariable : ScriptableObject, ISerializationCallbackReceiver
+    {
+       
+        [NonSerialized] public List<int2> Value;
+        public List<int2> InitialValue;
+        
+        public void OnBeforeSerialize() { }
+        public void OnAfterDeserialize()
+        {
+            Value = InitialValue.ToArray().ToList();
+        }
+        
+        public void Set(List<int2> value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Anhang/Abstranktions Sytem durch Game Events und Globale Varibalen Datein/Global Variablen/Vec2Variable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Utility.Variables
+{
+    [CreateAssetMenu(fileName = "Vec2Variable", menuName = "Utility/Varibles/Vec2")]
+    public class Vec2Variable : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [NonSerialized]
+        public float2 Value;
+        public float2 InitialValue;
+        
+        public void OnBeforeSerialize() { }
+        public void OnAfterDeserialize()
+        {
+            Value = InitialValue;
+        }
+        
+        public void Set(float2 value)
+        {
+            Value = value;
+        }
+    }
+}
